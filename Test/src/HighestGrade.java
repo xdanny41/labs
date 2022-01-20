aaf6dbf06374911bffa320da374dbdff7eb56314
xdanny41 <danny.nguyen-1@ou.edu>
2022-01-20 09:44:51 -0600

    Update HighestGrade.java

--- Test/src/HighestGrade.java
+++ Test/src/HighestGrade.java
@@ -3,7 +3,7 @@ public class HighestGrade {
 	
 	void output() {
 		
-		studentGrades printGrade = new studentGrades();
+		studentGrades printGrade = new studentGrades(); //outputs student grades and names by instantiation
 		studentNames printName = new studentNames();
 		
 		printGrade.output();

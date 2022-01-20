6f39f834a54f5c32bd9d88ac538a7a385ac5141c
xdanny41 <danny.nguyen-1@ou.edu>
2022-01-20 09:45:23 -0600

    Update Main.java

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 
-	HighestGrade findGrade = new HighestGrade();
+	HighestGrade findGrade = new HighestGrade(); //Outputs highest grade by instantiation
 	
 
 		findGrade.output();
